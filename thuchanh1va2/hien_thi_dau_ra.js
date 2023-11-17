8f16ed8b6d5e963b1d79debfdbcbe87392fe0c01
viettoan1020 <viettoann102003@gmail.com>
2023-11-17 15:44:44 +0700

    Tin nhan

---
+++ thuchanh1va2/hien_thi_dau_ra.js
@@ -0,0 +1,8 @@
+function showMessage(){
+    // alert('Xin chào!');
+    // document.getElementById('messageDiv').innerHTML='Xin chào!';
+    // document.write('Xin chào!');
+    console.log('Xin chào!');
+
+
+}
